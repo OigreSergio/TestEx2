3647ce619e84adc63b79bc6f486608648e771471
Sergio <sergio.hanganu@gmail.com>
2024-03-20 14:10:11 +0100

    push the test

--- Main.java
+++ Main.java
@@ -1,13 +1,6 @@
 import java.time.DayOfWeek;
 import java.time.Month;
 import java.time.OffsetDateTime;
-
-/*Crea un oggetto data da questa stringa 2023-03-01T13:00:00Z
-Ottieni l'anno
-Ottieni il mese
-Ottieni il giorno
-Ottieni il giorno della settimana
-Stampa i risultati sulla console -Crea dei test per questo esercizio*/
 public class Main {
     public static void main(String[] args) {
         OffsetDateTime data = OffsetDateTime.parse("2023-03-01T13:00:00Z");
--- MainTest.java
+++ MainTest.java
@@ -1,4 +1,32 @@
+import org.junit.jupiter.api.Test;
+
+import java.time.DayOfWeek;
+import java.time.Month;
+import java.time.OffsetDateTime;
 import static org.junit.jupiter.api.Assertions.*;
+
 class MainTest {
-  
+    @Test
+    public void Test_Year() {
+        OffsetDateTime yearTest = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        assertEquals(2023, Main.yearMethodh(yearTest));
+    }
+
+    @Test
+    public void Test_Month() {
+        OffsetDateTime monthTest = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        assertEquals(Month.MARCH, Main.monthMethodh(monthTest));
+    }
+
+    @Test
+    public void Test_Day_Of_Month() {
+        OffsetDateTime dayOfMonthTest = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        assertEquals(1, Main.dayOfMonthMethodh(dayOfMonthTest));
+    }
+
+    @Test
+    public void Test_Day_Of_Week() {
+        OffsetDateTime dayOfWheekTest = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        assertEquals(DayOfWeek.WEDNESDAY, Main.dayOfWeek(dayOfWheekTest));
+    }
 }
